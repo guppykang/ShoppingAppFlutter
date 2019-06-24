bc82f3732bb89a4f22e01d72b22f357f74597819
Joshua Kang <guppykang@gmail.com>
2019-06-24 01:34:05 -0700

    fixed back button bug

--- lib/pages/product.dart
+++ lib/pages/product.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class ProductPage extends StatelessWidget {
   final String imageUrl;
@@ -9,28 +10,34 @@ class ProductPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Center(
-          child: Column(
-            children: <Widget>[
-              Image.asset(imageUrl),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                child: Text(title),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: RaisedButton(
-                  color: Theme.of(context).accentColor,
-                  child: Text("Delete"),
-                  onPressed: () => Navigator.pop(context, true),
-                ),
-              )
-            ],
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.pop(context, false);
+        return Future.value(false);
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(title),
           ),
-        ));
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                Image.asset(imageUrl),
+                Container(
+                  padding: EdgeInsets.all(10.0),
+                  child: Text(title),
+                ),
+                Container(
+                  padding: EdgeInsets.all(10),
+                  child: RaisedButton(
+                    color: Theme.of(context).accentColor,
+                    child: Text("Delete"),
+                    onPressed: () => Navigator.pop(context, true),
+                  ),
+                )
+              ],
+            ),
+          )),
+    );
   }
 }
